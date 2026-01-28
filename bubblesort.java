3ace75236c655dbdc73477f925ae8f2cd15ab968
Shaik Samiulla <152060102+sami42838@users.noreply.github.com>
2026-01-28 21:13:19 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,33 @@
+package sorting;
+import java.util.*;
+public class bubble {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+     
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean swap=false;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swap=true;
+                    
+                }
+            }
+            if(!swap) break;
+        }
+        System.out.println("sorted array");
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }   
+}
